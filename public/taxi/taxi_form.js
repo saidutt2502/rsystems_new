72ca3442a2e3e335dae31aaaac6e88647a273375
Aman Sharma <armslive1904@gmail.com>
2018-10-30 00:10:17 +0530

    taxi airport chosen fixed

--- public/taxi/taxi_form.js
+++ public/taxi/taxi_form.js
@@ -14,11 +14,14 @@ $(document).ready(function(){
                     if(data.count>1)
                     {
 
-                        $('#add_form').append('<div class="remove"><div class="form-group"><br><label class="col-sm-3 control-label no-padding-right" >Place</label><div class="col-sm-4"><select id="airport_locations"  name="place" class="chosen-container chosen-container-single chosen-select"  required><option value="" disabled selected>Select Place</option></select></div></div><div class="form-group"><div class="col-sm-offset-3 col-sm-6"><input type="radio" name="journey" value="Drop" checked> Airport Drop</input></div><div class="col-sm-offset-3 col-sm-6"><input type="radio" name="journey" value="Pick"> Airport Pickup</input></div></div><div class="form-group"><label class="col-sm-3 control-label no-padding-right">Time</label><div class="col-sm-9"><input name="time" class="col-xs-10 col-sm-5" type="time" required></div></div></div>');
+                        $('#add_form').append('<div class="remove"><div class="form-group"><br><label class="col-sm-3 control-label no-padding-right" >Place</label><div class="col-sm-4"><select id="airport_locations"  name="place" class="chosen-container chosen-container-single chosen-select airport_list_chosen"  required><option value="" disabled selected>Select Place</option></select></div></div><div class="form-group"><div class="col-sm-offset-3 col-sm-6"><input type="radio" name="journey" value="Drop" checked> Airport Drop</input></div><div class="col-sm-offset-3 col-sm-6"><input type="radio" name="journey" value="Pick"> Airport Pickup</input></div></div><div class="form-group"><label class="col-sm-3 control-label no-padding-right">Time</label><div class="col-sm-9"><input name="time" class="col-xs-10 col-sm-5" type="time" required></div></div></div>');
                         for(var i=0;i<data.count-1;i++)
                         {
                             $('#airport_locations').append('<option value="'+data[i]+'">'+data[i]+'</option>');
                         }
+
+                        $('.airport_list_chosen').chosen();
+                        $('.airport_list_chosen').next().css({'width': $('.airport_list_chosen').parent().width()});
                         
                     }
                     else
