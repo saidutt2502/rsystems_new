940d65e95d424c2b2c16f8a53a907ea945a1753c
Saidutt <saiduttredkar@gmail.com>
2018-11-02 09:06:46 +0530

    reset password not fixed

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -101,17 +101,17 @@
 
 										<div class="toolbar clearfix">
 											<div>
-												<a href="#" data-target="#forgot-box" class="forgot-password-link">
+												<!-- <a href="#" data-target="#forgot-box" class="forgot-password-link">
 													<i class="ace-icon fa fa-arrow-left"></i>
 													I forgot my password
-												</a>
+												</a> -->
 											</div>
 
 											<div>
-												<a href="#" data-target="#signup-box" class="user-signup-link">
+												<!-- <a href="#" data-target="#signup-box" class="user-signup-link">
 													I want to register
 													<i class="ace-icon fa fa-arrow-right"></i>
-												</a>
+												</a> -->
 											</div>
 										</div>
 									</div><!-- /.widget-body -->
